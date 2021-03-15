1ad23e91b591673a3893ed3f696810b2775829c7
smygw72 <smygw72@gmail.com>
2021-03-16 01:18:07 +0900

    finished Chpater 10

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    """拡張ユーザーモデル"""
+
+    class Meta:
+        verbose_name_plural = 'CustomUser'
