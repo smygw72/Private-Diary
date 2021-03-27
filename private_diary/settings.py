67b2170dc0c637d15a7f60ea01622c16eb30885a
smygw72 <smygw72@gmail.com>
2021-03-27 22:41:26 +0900

    fix bugs in previous commit

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -14,8 +14,8 @@ STATIC_ROOT = '/user/share/nginx/html/static'
 MEDIA_ROOT = '/user/share/nginx/html/media'
 
 # Amazon SES関連設定
-AWS_SES_ACCESS_KEY_ID = os.environ('AWS_SES_ACCESS_KEY_ID')
-AWS_SES_SECRET_ACCESS_KEY = os.environ('AWS_SES_SECRET_ACCESS_KEY')
+AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
+AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
 EMAIL_BACKEND = 'django_ses.SESBackend'
 
 # ロギング
